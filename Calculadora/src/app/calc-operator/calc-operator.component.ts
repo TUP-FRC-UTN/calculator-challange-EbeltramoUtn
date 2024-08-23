5fc331156422562b535388a1aa1076f6e57571f7
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-08-23 19:05:50 -0300

    Finalizado

--- Calculadora/src/app/calc-operator/calc-operator.component.ts
+++ Calculadora/src/app/calc-operator/calc-operator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-calc-operator',
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './calc-operator.component.css'
 })
 export class CalcOperatorComponent {
+  @Input() numberA: number = 0;
+  @Input() numberB: number = 0;
+  @Output() resultEvent = new EventEmitter<number>();
+  @Output() clearEvent = new EventEmitter<void>();
 
+  add(){
+    this.resultEvent.emit( Number(this.numberA) + Number(this.numberB));
+  }
+  substract(){
+    this.resultEvent.emit(Number(this.numberA) - Number(this.numberB));
+  }
+  powerOf(){
+    this.resultEvent.emit(Number(this.numberA) ** Number(this.numberB));
+  }
+  multiply(){
+    this.resultEvent.emit(Number(this.numberA) * Number(this.numberB));
+  }
+  divide(){
+    this.numberB == 0 ? this.resultEvent.emit(0) : this.resultEvent.emit(Number(this.numberA) / Number(this.numberB));
+  }
+  clear(){
+    this.clearEvent.emit();
+  }
 }
--- Calculadora/src/app/calc-view/calc-view.component.ts
+++ Calculadora/src/app/calc-view/calc-view.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
+import { CalcOperatorComponent } from '../calc-operator/calc-operator.component';
 
 @Component({
   selector: 'app-calc-view',
   standalone: true,
-  imports: [],
+  imports: [CalcOperatorComponent],
   templateUrl: './calc-view.component.html',
   styleUrl: './calc-view.component.css'
 })
 export class CalcViewComponent {
   numberA : number = 0;
   numberB : number = 0;
+  result: number=0;
 
   valorA(event : any){
     this.numberA=event.target.value
   }
   valorB(event : any){
     this.numberB=event.target.value
+
+  }
+  updateResult(result:number){
+    this.result = result;
+  }
+  clear(){
+    this.result = 0;
+    this.numberA=0;
+    this.numberB=0;
 
   }
 }
